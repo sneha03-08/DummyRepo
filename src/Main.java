7a270c314e9847241e4363579741f04ad7ba5a08
snehamathadawar <2ke19cs104@kleit.ac.in>
2024-07-12 15:20:00 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello from mathadawar01@gmail.com")
+        System.out.println("Hello from mathadawar01@gmail.com");
+        System.out.println("hello from 2ke19cssss");
+
     }
 }
--- src/demo.java
+++ src/demo.java
@@ -1,5 +1,5 @@
 public class demo{
     public static void main(String[] args){
-        System.out.println("demo file created from mathaaa")
+        System.out.println("demo file created from mathaaa");
     }
 }
